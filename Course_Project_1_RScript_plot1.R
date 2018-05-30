c9becdcfa46bb5b1ee9946c51e78ecc6acd70247
Saima Aziz <32683029+SaimaAzizKhan@users.noreply.github.com>
2018-05-30 01:13:01 -0500

    Update Course_Project_1_RScript_plot1.R

--- Course_Project_1_RScript_plot1.R
+++ Course_Project_1_RScript_plot1.R
@@ -1,1 +1,16 @@
 
+UCepc<- read.table("~/Desktop/test_space/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+
++ 
+
++ 
+
++projset<- subset(UCepc, Date == "1/2/2007" | Date == "2/2/2007") 
+
++GAP <- as.numeric(projset$Global_active_power) 
+
++png("plot1.png", height = 480, width = 480) 
+
++hist(GAP, col = "red", main = "Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "Frequency") 
+
++dev.off()
